fff581d12a2bab47c05f9b235527f59b288774a1
Ofer Lifshes <lifshesofer@gmail.com>
2024-10-06 14:10:33 +0300

    initial push

---
+++ PlaywrightTest/Models/GmailHome.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Playwright;
+
+namespace PlaywrightTest.Models
+{
+    public class GmailHomePage(IPage page)
+    {
+        private readonly IPage _page = page;
+        public const string Url = "https://gmail.com";
+        private readonly ILocator _signInButton = page.Locator("[data-action=\"sign in\"]");
+
+        public async Task GotoAsync()
+        {
+            await _page.GotoAsync(Url);
+        }
+
+        public async Task ClickSignIn()
+        {
+            await _signInButton.ClickAsync();
+        }
+    }
+}
