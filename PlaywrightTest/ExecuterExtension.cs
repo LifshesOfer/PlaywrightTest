1f4f708914286050e26100eff8beedeee8121fd1
Ofer Lifshes <lifshesofer@gmail.com>
2024-10-07 13:00:00 +0300

    added extension method for nicer (but not yet perfect) fluent syntax

---
+++ PlaywrightTest/ExecuterExtension.cs
@@ -0,0 +1,11 @@
+﻿namespace PlaywrightTest
+{
+    public static class ExecuterExtension
+    {
+        public static async Task<TOut> Then<TIn, TOut>(this Task<TIn> inputTask, Func<TIn, Task<TOut>> followedByFunc)
+        {
+            var inputResult = await inputTask;
+            return await followedByFunc(inputResult); // Added null-forgiving operator
+        }
+    }
+}
