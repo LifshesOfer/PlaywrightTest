e6afbf09b4bf9d4c02064407883469afff90f386
Ofer Lifshes <lifshesofer@gmail.com>
2024-10-07 13:00:17 +0300

    added users test data

---
+++ PlaywrightTest/TestData/TestUsers.cs
@@ -0,0 +1,12 @@
+﻿using PlaywrightTest.Models;
+
+namespace PlaywrightTest.TestData
+{
+    public static class TestUsers
+    {
+        public static readonly User TrueUser = new User("mctestersontester554@gmail.com", "!@#123QWEqwe");
+        public static readonly User UserBadPass = new User("mctestersontester554@gmail.com", "12345678");
+        public static readonly User WrongUser = new User("donatester@gmail.com", "12345678");
+
+    }
+}
