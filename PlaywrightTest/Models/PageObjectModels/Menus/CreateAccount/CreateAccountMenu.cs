d29b046107e6478ad8f9e0a289467b4fdabb413a
Ofer Lifshes <lifshesofer@gmail.com>
2024-10-10 12:44:12 +0300

    minor changes to locators

--- PlaywrightTest/Models/PageObjectModels/Menus/CreateAccount/CreateAccountMenu.cs
+++ PlaywrightTest/Models/PageObjectModels/Menus/CreateAccount/CreateAccountMenu.cs
@@ -5,8 +5,8 @@ namespace PlaywrightTest.Models.PageObjectModels.Menus.CreateAccount
 {
     internal class CreateAccountMenu(IPage page) : PageObject(page)
     {
-        public readonly ILocator DropdownList = page.Locator("//ul[@role='menu'][@aria-label='Create account']");
-        public readonly ILocator MenuItems = page.Locator("role='menuitem'");
+        public readonly ILocator DropdownList = page.GetByLabel("Create account");
+        public readonly ILocator MenuItems = page.GetByRole(AriaRole.Menuitem);
         public ILocator MenuItemOption(string optionText) => MenuItems.Locator($"//li[text()='{optionText}']");
         public async Task SelectOption(string optionText)
         {
