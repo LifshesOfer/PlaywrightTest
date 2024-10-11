41e32843131a68cd91d50e2899c768fc8d7e41ee
Ofer Lifshes <lifshesofer@gmail.com>
2024-10-11 10:18:31 +0300

    added centralized sql injections class

---
+++ PlaywrightTest/TestData/SqlInjections.cs
@@ -0,0 +1,9 @@
+﻿namespace PlaywrightTest.TestData
+{
+    public static class SqlInjections
+    {
+        public static readonly string SingleQuote = "'";
+        public static readonly string TrueStatement = "'1=1";
+        public static readonly string FullQuery = $"SELECT * FROM users WHERE username='{TestUsers.TrueUser.UserName}' AND password='{TestUsers.TrueUser.Password}'";
+    }
+}
