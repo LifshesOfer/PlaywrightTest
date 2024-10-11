5714e39766b38c9cd4cdc8eac927f6a007ff72e2
Ofer Lifshes <lifshesofer@gmail.com>
2024-10-11 10:18:22 +0300

    added centralized error messages class

---
+++ PlaywrightTest/TestData/ErrorMessages.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlaywrightTest.TestData
+{
+    public static class ErrorMessages
+    {
+        public const string NoUserName = "Enter an email or phone number";
+        public const string InvalidUserName = "Enter a valid email or phone number";
+        public const string IncorrectUserName = "Couldn’t find your Google Account";
+        public const string IncorrectPassword = "Wrong password. Try again or click Forgot password to reset it.";
+
+    }
+}
