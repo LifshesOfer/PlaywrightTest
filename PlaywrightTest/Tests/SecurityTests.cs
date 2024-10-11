9b20c6e1975a0c8b0acfc4dd11734e20b095c203
Ofer Lifshes <lifshesofer@gmail.com>
2024-10-11 10:19:25 +0300

    added security tests

---
+++ PlaywrightTest/Tests/SecurityTests.cs
@@ -0,0 +1,49 @@
+﻿using PlaywrightTest.Models.PageObjectModels;
+using PlaywrightTest.TestData;
+
+namespace PlaywrightTest.Tests
+{
+    [Parallelizable(ParallelScope.Self)]
+    [TestFixture]
+    public class SecurityTests : BaseTest
+    {
+        
+        public static object[] UserSqlInjectionData = 
+        {
+            new object[] { SqlInjections.SingleQuote, ErrorMessages.IncorrectUserName },
+            new object[] { SqlInjections.TrueStatement, ErrorMessages.IncorrectUserName },
+            new object[] { SqlInjections.FullQuery, ErrorMessages.InvalidUserName }
+        };
+
+        public static object[] PasswordSqlInjectionData = {
+
+            new object[] { SqlInjections.SingleQuote, ErrorMessages.IncorrectPassword },
+            new object[] { SqlInjections.TrueStatement, ErrorMessages.IncorrectPassword },
+            new object[] { SqlInjections.FullQuery, ErrorMessages.IncorrectPassword }
+        };
+
+        [Test]
+        [TestCaseSource(nameof(UserSqlInjectionData))]
+        public async Task CheckSqlInUserName_ExpectErrorMessage(string sql, string expected)
+        {
+            await HomePage.GotoAsync(Page)
+                .Then(homePage => homePage.ClickSignIn())
+                .Then(loginPage => loginPage.EnterUsername(sql))
+                .Then(loginPage => loginPage.ContinueToPasswordInput());
+
+            await Expect(Page.GetByText(expected)).ToBeVisibleAsync();
+        }
+
+        [Test]
+        [TestCaseSource(nameof(PasswordSqlInjectionData))]
+        public async Task CheckSqlInPassword_ExpectErrorMessage(string sql, string expected)
+        {
+            var userName = TestUsers.TrueUser.UserName;
+            await HomePage.GotoAsync(Page)
+                .Then(homePage => homePage.ClickSignIn())
+                .Then(LoginPage => LoginPage.Login(userName, sql));
+
+            await Expect(Page.GetByText(expected)).ToBeVisibleAsync();
+        }
+    }
+}
